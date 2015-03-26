d894d86dd7ec4b8d4730dfa69883b243cb34c7df
Sultanxda <sultanxda@gmail.com>
2015-03-26 00:19:30 -0700

    msm: ion: Sync with CAF LA.BF.1.1.1_rb1.17

--- drivers/gpu/ion/ion_page_pool.c
+++ drivers/gpu/ion/ion_page_pool.c
@@ -22,7 +22,6 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
-#include <linux/swap.h>
 #include "ion_priv.h"
 
 struct ion_page_pool_item {
@@ -33,7 +32,6 @@ struct ion_page_pool_item {
 static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 {
 	struct page *page;
-	struct scatterlist sg;
 
 	page = alloc_pages(pool->gfp_mask & ~__GFP_ZERO, pool->order);
 
@@ -43,11 +41,6 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 	if (pool->gfp_mask & __GFP_ZERO)
 		if (ion_heap_high_order_page_zero(page, pool->order))
 			goto error_free_pages;
-
-	sg_init_table(&sg, 1);
-	sg_set_page(&sg, page, PAGE_SIZE << pool->order, 0);
-	sg_dma_address(&sg) = sg_phys(&sg);
-	dma_sync_sg_for_device(NULL, &sg, 1, DMA_BIDIRECTIONAL);
 
 	return page;
 error_free_pages:
@@ -105,22 +98,25 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 	return page;
 }
 
-void *ion_page_pool_alloc(struct ion_page_pool *pool)
+void *ion_page_pool_alloc(struct ion_page_pool *pool, bool *from_pool)
 {
 	struct page *page = NULL;
 
 	BUG_ON(!pool);
 
-	mutex_lock(&pool->mutex);
-	if (pool->high_count)
-		page = ion_page_pool_remove(pool, true);
-	else if (pool->low_count)
-		page = ion_page_pool_remove(pool, false);
-	mutex_unlock(&pool->mutex);
+	*from_pool = true;
 
-	if (!page)
+	if (mutex_trylock(&pool->mutex)) {
+		if (pool->high_count)
+			page = ion_page_pool_remove(pool, true);
+		else if (pool->low_count)
+			page = ion_page_pool_remove(pool, false);
+		mutex_unlock(&pool->mutex);
+	}
+	if (!page) {
 		page = ion_page_pool_alloc_pages(pool);
-
+		*from_pool = false;
+	}
 	return page;
 }
 
@@ -150,10 +146,7 @@ int ion_page_pool_shrink(struct ion_page_pool *pool, gfp_t gfp_mask,
 	int i;
 	bool high;
 
-	if (current_is_kswapd())
-		high = 1;
-	else
-		high = !!(gfp_mask & __GFP_HIGHMEM);
+	high = gfp_mask & __GFP_HIGHMEM;
 
 	if (nr_to_scan == 0)
 		return ion_page_pool_total(pool, high);
@@ -162,10 +155,10 @@ int ion_page_pool_shrink(struct ion_page_pool *pool, gfp_t gfp_mask,
 		struct page *page;
 
 		mutex_lock(&pool->mutex);
-		if (pool->low_count) {
-			page = ion_page_pool_remove(pool, false);
-		} else if (high && pool->high_count) {
+		if (high && pool->high_count) {
 			page = ion_page_pool_remove(pool, true);
+		} else if (pool->low_count) {
+			page = ion_page_pool_remove(pool, false);
 		} else {
 			mutex_unlock(&pool->mutex);
 			break;
