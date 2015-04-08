a81e4f2c8f973e0e4731aecd1e86975783a6766c
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-08 08:05:54 -0500

    camera_v2: track our own camera drivers
    Change-Id: Ib59a891746dfd23616741c6fb8d491da051fc0ae

--- drivers/media/platform/msm/camera_v2/sensor/csiphy/msm_csiphy.c
+++ drivers/media/platform/msm/camera_v2/sensor/csiphy/msm_csiphy.c
@@ -15,6 +15,7 @@
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/module.h>
+#include <linux/ratelimit.h>
 #include <linux/irqreturn.h>
 #include <mach/vreg.h>
 #include "msm_csiphy.h"
@@ -22,7 +23,6 @@
 #include "msm_csiphy_hwreg.h"
 #include "msm_camera_io_util.h"
 #define DBG_CSIPHY 0
-//#define CONFIG_MSMB_CAMERA_DEBUG
 
 #define V4L2_IDENT_CSIPHY                        50003
 #define CSIPHY_VERSION_V22                        0x01
@@ -47,7 +47,6 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 	uint16_t lane_mask = 0;
 	void __iomem *csiphybase;
 	uint8_t csiphy_id = csiphy_dev->pdev->id;
-	uint16_t irq2 = 0;
 	csiphybase = csiphy_dev->base;
 	if (!csiphybase) {
 		pr_err("%s: csiphybase NULL\n", __func__);
@@ -126,31 +125,6 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 		j++;
 		lane_mask >>= 1;
 	}
-
-/*                                                                                                            */
-/*                                                                                                       */
-#if defined (CONFIG_HI351) || defined (CONFIG_HI543) || defined (CONFIG_HI544)
-	pr_err("%s padding the offset\n", __func__);
-	if(csiphy_dev->pdev->id == 0){//main camera
-		if(csiphy_dev->hw_version < CSIPHY_VERSION_V30){	//8x10, hi351
-			msm_camera_io_w(0xc, csiphy_dev->base + MIPI_CSIPHY_LNCK_CFG4_ADDR);			/*                                                                                              */
-			irq2 = msm_camera_io_r(csiphy_dev->base + MIPI_CSIPHY_LNCK_CFG4_ADDR);
-			pr_err("%s MIPI_CSIPHY_LNCK_CFG4_ADDR = 0x%x hw_ver : 0x%x\n", __func__, irq2, csiphy_dev->hw_version);
-		}else{	//8x26, hi543
-			msm_camera_io_w(0xc, csiphy_dev->base + MIPI_CSIPHY_LNn_CFG4_ADDR + 0x40);		/*                                                                         */
-			irq2 = msm_camera_io_r(csiphy_dev->base + MIPI_CSIPHY_LNn_CFG4_ADDR + 0x40);
-			pr_err("%s MIPI_CSIPHY_LNn_CFG4_ADDR = 0x%x hw_ver : 0x%x\n", __func__, irq2, csiphy_dev->hw_version);
-		}
-	}else{
-		irq2 = 0;
-	}
-#else
-	irq2 = 0;
-#endif
-/*                                                                                                       */
-/*                                                                                                            */
-
-
 	return rc;
 }
 
@@ -200,13 +174,12 @@ static struct msm_cam_clk_info csiphy_8960_clk_info[] = {
 	{"csiphy_timer_src_clk", 177780000},
 	{"csiphy_timer_clk", -1},
 };
-/*                                                                                     */ /*                                                         */
+
 static struct msm_cam_clk_info csiphy_8610_clk_info[] = {
 	{"csiphy_timer_src_clk", 200000000},
 	{"csiphy_timer_clk", -1},
 	{"csi_ahb_clk", -1},
 };
-/*                                                                                     */
 
 static struct msm_cam_clk_info csiphy_8974_clk_info[] = {
 	{"camss_top_ahb_clk", -1},
@@ -627,7 +600,7 @@ static long msm_csiphy_cmd(struct csiphy_device *csiphy_dev, void *arg)
 		rc = msm_csiphy_release(csiphy_dev, &csi_lane_params);
 		break;
 	default:
-		pr_err("%s: %d failed\n", __func__, __LINE__);
+		pr_err_ratelimited("%s: %d failed\n", __func__, __LINE__);
 		rc = -ENOIOCTLCMD;
 		break;
 	}
